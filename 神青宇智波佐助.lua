e8854634cff5666eca5c8a16fe5c9e1b2e8c4ea6
gycgchgyfytdttr <18058643061@163.com>
2024-12-15 23:38:11 +0800

    Add files via upload

---
+++ 神青宇智波佐助.lua
@@ -0,0 +1,171 @@
+if game.PlaceId == 155615604 then
+local headerText = "检查到是监狱人生，正在启动监狱人生脚本😎😎😎"
+local textColor = Color3.fromRGB(0,255,0) 
+local fadeDuration = 2 
+local displayDuration = 2 
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+local Label = Instance.new("TextLabel")
+Label.Parent = ScreenGui
+Label.Size = UDim2.new(0.5, 0, 1, 0)
+Label.Position = UDim2.new(0.25, 0, 0.0001, 0)
+Label.Text = headerText
+Label.TextColor3 = textColor
+Label.TextTransparency = 1 
+Label.TextScaled = true
+Label.BackgroundTransparency = 1
+Label.Font = Enum.Font.Unknown 
+
+for i = 0.5, 20 do
+    Label.TextTransparency = 1 - (i / 20)
+    wait(fadeDuration / 20)
+end
+
+wait(displayDuration)
+
+for i = 0.5, 20 do
+    Label.TextTransparency = i / 20
+    wait(fadeDuration / 20)
+end
+
+ScreenGui:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gycgchgyfytdttr/shenqin/refs/heads/main/%E7%A5%9E%E9%9D%92%E7%9B%91%E7%8B%B1%E4%BA%BA%E7%94%9F.lua"))()
+elseif game.PlaceId == 7239319209 then
+local headerText = "检查到是俄亥俄州，正在启动俄亥俄州脚本😎😎😎"
+local textColor = Color3.fromRGB(0,255,0) 
+local fadeDuration = 2 
+local displayDuration = 2 
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+local Label = Instance.new("TextLabel")
+Label.Parent = ScreenGui
+Label.Size = UDim2.new(0.5, 0, 1, 0)
+Label.Position = UDim2.new(0.25, 0, 0.0001, 0)
+Label.Text = headerText
+Label.TextColor3 = textColor
+Label.TextTransparency = 1 
+Label.TextScaled = true
+Label.BackgroundTransparency = 1
+Label.Font = Enum.Font.Unknown 
+
+for i = 0.5, 20 do
+    Label.TextTransparency = 1 - (i / 20)
+    wait(fadeDuration / 20)
+end
+
+wait(displayDuration)
+
+for i = 0.5, 20 do
+    Label.TextTransparency = i / 20
+    wait(fadeDuration / 20)
+end
+
+ScreenGui:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gycgchgyfytdttr/shenqin/refs/heads/main/%E7%A5%9E%E9%9D%92%E4%BF%84%E4%BA%A5%E4%BF%84%E5%B7%9E%E8%84%9A%E6%9C%AC.lua"))()
+elseif game.PlaceId == 3623096087 then
+local headerText = "检查到是力量传奇，正在启动力量传奇脚本😎😎😎"
+local textColor = Color3.fromRGB(0,255,0) 
+local fadeDuration = 2 
+local displayDuration = 2 
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+local Label = Instance.new("TextLabel")
+Label.Parent = ScreenGui
+Label.Size = UDim2.new(0.5, 0, 1, 0)
+Label.Position = UDim2.new(0.25, 0, 0.0001, 0)
+Label.Text = headerText
+Label.TextColor3 = textColor
+Label.TextTransparency = 1 
+Label.TextScaled = true
+Label.BackgroundTransparency = 1
+Label.Font = Enum.Font.Unknown 
+
+for i = 0.5, 20 do
+    Label.TextTransparency = 1 - (i / 20)
+    wait(fadeDuration / 20)
+end
+
+wait(displayDuration)
+
+for i = 0.5, 20 do
+    Label.TextTransparency = i / 20
+    wait(fadeDuration / 20)
+end
+
+ScreenGui:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gycgchgyfytdttr/shenqin/refs/heads/main/%E7%A5%9E%E9%9D%92%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87.lua"))()
+elseif game.PlaceId == 3956818381 then
+local headerText = "检查到是忍者传奇，正在启动忍者传奇脚本😎😎😎"
+local textColor = Color3.fromRGB(0,255,0) 
+local fadeDuration = 2 
+local displayDuration = 2 
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+local Label = Instance.new("TextLabel")
+Label.Parent = ScreenGui
+Label.Size = UDim2.new(0.5, 0, 1, 0)
+Label.Position = UDim2.new(0.25, 0, 0.0001, 0)
+Label.Text = headerText
+Label.TextColor3 = textColor
+Label.TextTransparency = 1 
+Label.TextScaled = true
+Label.BackgroundTransparency = 1
+Label.Font = Enum.Font.Unknown 
+
+for i = 0.5, 20 do
+    Label.TextTransparency = 1 - (i / 20)
+    wait(fadeDuration / 20)
+end
+
+wait(displayDuration)
+
+for i = 0.5, 20 do
+    Label.TextTransparency = i / 20
+    wait(fadeDuration / 20)
+end
+
+ScreenGui:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gycgchgyfytdttr/shenqin/refs/heads/main/%E7%A5%9E%E9%9D%92%E5%BF%8D%E8%80%85%E4%BC%A0%E5%A5%87.lua"))()
+else
+local headerText = "检查到未上传脚本，正在开启通用😎😎😎"
+local textColor = Color3.fromRGB(0,255,0) 
+local fadeDuration = 2 
+local displayDuration = 2 
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+local Label = Instance.new("TextLabel")
+Label.Parent = ScreenGui
+Label.Size = UDim2.new(0.5, 0, 1, 0)
+Label.Position = UDim2.new(0.25, 0, 0.0001, 0)
+Label.Text = headerText
+Label.TextColor3 = textColor
+Label.TextTransparency = 1 
+Label.TextScaled = true
+Label.BackgroundTransparency = 1
+Label.Font = Enum.Font.Unknown 
+
+for i = 0.5, 20 do
+    Label.TextTransparency = 1 - (i / 20)
+    wait(fadeDuration / 20)
+end
+
+wait(displayDuration)
+
+for i = 0.5, 20 do
+    Label.TextTransparency = i / 20
+    wait(fadeDuration / 20)
+end
+
+ScreenGui:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gycgchgyfytdttr/shenqin/refs/heads/main/%E7%A5%9E%E9%9D%92%E8%84%9A%E6%9C%AC%E9%80%9A%E7%94%A8.lua"))()
+end
